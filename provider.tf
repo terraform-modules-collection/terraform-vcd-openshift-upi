4760c782719a9bcc23432f48667952c4d9ceac50
Igor <idyurev@gmail.com>
2020-05-19 18:16:31 +0300

    v0.1.5

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ provider "vcd" {
   user                 = var.vcdUsername
   password             = var.vcdPassword
   org                  = var.vcdOrganization
-  url                  = var.vcdOrganization
+  url                  = var.vcdUrl
   vdc                  = var.vcdVirtualDC
   allow_unverified_ssl = true
 }
